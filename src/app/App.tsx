f8813c59b0f36f60e4b536e190855719002b867e
Denis <denisatamankin@yandex.ru>
2025-04-09 15:08:28 +0500

    B

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -41,6 +41,11 @@ export const App = () => {
 					}
 				/>
 			</Routes>
+			<button
+				className={styles.link}
+				onClick={() => (window as any).ym(100911732, 'reachGoal', 'buy')}>
+				Купить
+			</button>
 		</div>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app';
-import { Analytic } from './context/analityc';
+// import { Analytic } from './context/analityc';
 import './styles.css';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 root.render(
 	<StrictMode>
-        <BrowserRouter
-            basename={process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/'}>
-            <App />
-        </BrowserRouter>
-    </StrictMode>
+		<BrowserRouter
+			basename={process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/'}>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
 );
